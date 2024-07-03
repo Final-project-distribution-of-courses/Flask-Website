4a5c6a970f6d0f9269efe7a6e71b16f80c1ec467
Renana Turgeman <renanaturgeman058@gmail.com>
2024-07-03 16:45:00 +0300

    add select box

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,18 +11,18 @@ def home():
 
 @app.route('/process', methods=['POST'])
 def process():
-    # קבלת הקלט מהטופס
-    name = request.form['name']
-    course1 = request.form['course1']
-    course2 = request.form['course2']
-    course3 = request.form['course3']
+    # Get the input from the form
+    name = request.form.get('name')
+    course1 = request.form.get('course1')
+    course2 = request.form.get('course2')
+    course3 = request.form.get('course3')
 
-    # כאן תוכל להפעיל את האלגוריתם על הקלט וליצור את הפלט
-    # לשם הדוגמה, ניצור פלט פשוט
-    output = f"{name}, הקורסים שהוקצו לך הם: {course1}, {course2}, {course3}"
+    # Example output for demonstration
+    output = f"{name}, the courses allocated to you are: {course1}, {course2}, {course3}"
 
     return render_template('result.html', name=name, output=output)
 
 
 if __name__ == '__main__':
+    os.environ['FLASK_ENV'] = 'development'
     app.run(debug=True)
