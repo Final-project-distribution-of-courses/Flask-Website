dc5db2d99d264c0d5dccb4daa644ac777a7753ee
Renana Turgeman <renanaturgeman058@gmail.com>
2024-07-03 17:11:01 +0300

    create new form to ACEEI

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/form', methods=['GET', 'POST'])
+def algorithm_form():
+    algorithm = request.args.get('algorithm')
 
-@app.route('/process', methods=['POST'])
-def process():
-    # Get the input from the form
-    name = request.form.get('name')
-    course1 = request.form.get('course1')
-    course2 = request.form.get('course2')
-    course3 = request.form.get('course3')
-
-    # Example output for demonstration
-    output = f"{name}, the courses allocated to you are: {course1}, {course2}, {course3}"
-
-    return render_template('result.html', name=name, output=output)
-
+    if algorithm == 'aceei':
+        return render_template('aceei_form.html')
+    elif algorithm == 'manipulation':
+        return render_template('manipulation_form.html')
+    elif algorithm == 'tabusearch':
+        return render_template('tabusearch_form.html')
+    else:
+        # Handle invalid algorithm selection
+        return "Invalid algorithm selection"
 
 if __name__ == '__main__':
-    os.environ['FLASK_ENV'] = 'development'
     app.run(debug=True)
